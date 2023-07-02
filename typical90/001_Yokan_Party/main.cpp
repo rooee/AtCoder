bc736f1103ba17763175e7a129f91f02250c371a
rooee <rujinami@gmail.com>
2023-07-02 10:31:23 +0900

    (later) Typical90 001_Yokan_Party

---
+++ typical90/001_Yokan_Party/main.cpp
@@ -0,0 +1,12 @@
+// https://atcoder.jp/contests/typical90/tasks/typical90_a
+
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+
+  int a, b, c;
+  string s;
+  cin >> a >> b >> c >> s ;
+  cout << a+b+c << " " << s << endl;
+}
