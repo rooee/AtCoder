ef05b5aea6943bac3b1e83bc93b89264138236a2
rooee <rujinami@gmail.com>
2023-07-06 19:34:39 +0900

    (imcomplete)ABC104_C_All_Green

--- The_Ant_Book_Elementary/2023-0702-05_ABC104_C_All_Green/main.cpp
+++ The_Ant_Book_Elementary/2023-0702-05_ABC104_C_All_Green/main.cpp
@@ -4,18 +4,51 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-int main() {
+void sub2 (const vector<int> &nump, const vector<int> &bonus, const int &D,
+           const int &G, vector<int> solved, int &maxval) {
+  int points = 0, counts = 0;
+  for (int i = 0; i < D; ++i) {
+    counts += solved.at (i);
+    points += (i + 1) * 100 * solved.at (i);
+    if (solved.at (i) == nump.at (i)) {
+      points += bonus.at (i);
+    }
+  }
+  if (G <= points) {
+    if (counts < maxval) {
+      maxval = counts;
+    }
+/*
+    for (int i = 0; i < D; ++i) {
+      cout << solved.at (i) << ", ";
+    }
+    cout << endl;
+    cout << "point=" << points << ", counts=" << counts << endl;
+*/
+  }
+}
 
-  int D;
-  int64_t G; 
+void sub (const vector<int> &nump, const vector<int> &bonus, const int &D,
+          const int &G, int l, vector<int> solved, int &maxval) {
+  if (l == D) {
+    sub2 (nump, bonus, D, G, solved, maxval);
+    return;
+  }
+  for (int i = 0; i < nump.at (l) + 1; ++i) {
+    vector<int> tmp = solved;
+    tmp.push_back (i);
+    sub (nump, bonus, D, G, l + 1, tmp, maxval);
+  }
+}
+
+int main () {
+  int D, G;
   cin >> D >> G;
-  vector<pair<int, int>> v(D, pair<int, int>(0,0));
-  for (int i=0;i<D;++i){
-    cin >> v.at(i).first >> v.at(i).second;
+  vector<int> nump (D), bonus (D), solved;
+  for (int i = 0; i < D; ++i) {
+    cin >> nump.at (i) >> bonus.at (i);
   }
-
-  for (auto a: v){
-    cout << a.first << ", " << a.second << endl;;
-  }
-
+  int maxval = accumulate (nump.begin (), nump.end (), 0);
+  sub (nump, bonus, D, G, 0, solved, maxval);
+  cout << maxval << endl;
 }
