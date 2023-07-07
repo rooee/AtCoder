5b7e69f5133159825ed25d7884c4fc0d16af7952
rooee <rujinami@gmail.com>
2023-07-07 20:59:50 +0900

    (TLE)ABC104_C_All_Green

--- The_Ant_Book_Elementary/2023-0702-05_ABC104_C_All_Green/main.cpp
+++ The_Ant_Book_Elementary/2023-0702-05_ABC104_C_All_Green/main.cpp
@@ -4,46 +4,34 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-void evaluation (const vector<int> &nump, const vector<int> &bonus, 
-                const int &D, const int &G, 
-                vector<int> vcontainer, int &maxval) {
-  int points = 0, counts = 0;
-  for (int i = 0; i < D; ++i) {
-    counts += vcontainer.at (i);
-    points += (i + 1) * 100 * vcontainer.at (i);
-    if (vcontainer.at (i) == nump.at (i)) {
-      points += bonus.at (i);
-    }
-  }
-  if (G <= points) {
-    if (counts < maxval) {
-      maxval = counts;
-    }
-  }
-}
-
 void build_vcontainer (const vector<int> &nump, const vector<int> &bonus, 
                        const int &D, const int &G, 
-                       int level, vector<int> vcontainer, int &maxval) {
+                       int level, int &maxval, int points, int counts) {
   if (level == D) {
-    evaluation(nump, bonus, D, G, vcontainer, maxval);
+    if (G <= points) {
+      if (counts < maxval) {
+        maxval = counts;
+      }
+    }
     return;
   }
   for (int i = 0; i < nump.at (level) + 1; ++i) {
-    vector<int> tmp = vcontainer;
-    tmp.at(level)=i;
-    build_vcontainer (nump, bonus, D, G, level + 1, tmp, maxval);
+    int tmpppoints=(level+1)*100*i;
+    if (i == nump.at (level)) {
+      tmpppoints += bonus.at (level);
+    }
+    build_vcontainer (nump, bonus, D, G, level + 1, maxval, points+tmpppoints, counts+i);
   }
 }
 
 int main () {
   int D, G;
   cin >> D >> G;
-  vector<int> nump (D), bonus (D), vcontainer(D);
+  vector<int> nump (D), bonus (D);
   for (int i = 0; i < D; ++i) {
     cin >> nump.at (i) >> bonus.at (i);
   }
   int maxval = accumulate (nump.begin (), nump.end (), 0);
-  build_vcontainer (nump, bonus, D, G, 0, vcontainer, maxval);
+  build_vcontainer (nump, bonus, D, G, 0, maxval, 0,0);
   cout << maxval << endl;
 }
