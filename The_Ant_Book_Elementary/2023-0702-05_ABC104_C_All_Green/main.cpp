897b182871bc0789a20fb6562a45fc5b12648f92
rooee <rujinami@gmail.com>
2023-07-09 22:46:21 +0900

    (not yet)ABC104_C_All_Green

--- The_Ant_Book_Elementary/2023-0702-05_ABC104_C_All_Green/main.cpp
+++ The_Ant_Book_Elementary/2023-0702-05_ABC104_C_All_Green/main.cpp
@@ -1,37 +1,50 @@
 // https://atcoder.jp/contests/abc104/tasks/abc104_c
 // https://qiita.com/drken/items/e77685614f3c6bf86f44
+// https://atcoder.jp/contests/abc104/submissions/43330580
 
 #include <bits/stdc++.h>
 using namespace std;
 
-void build_vcontainer (const vector<int> &nump, const vector<int> &bonus, 
-                       const int &D, const int &G, 
-                       int level, int &maxval, int points, int counts) {
-  if (level == D) {
-    if (G <= points) {
-      if (counts < maxval) {
-        maxval = counts;
-      }
-    }
-    return;
-  }
-  for (int i = 0; i < nump.at (level) + 1; ++i) {
-    int tmpppoints=(level+1)*100*i;
-    if (i == nump.at (level)) {
-      tmpppoints += bonus.at (level);
-    }
-    build_vcontainer (nump, bonus, D, G, level + 1, maxval, points+tmpppoints, counts+i);
-  }
-}
+const int INF=1<<29;
+
 
 int main () {
   int D, G;
   cin >> D >> G;
+  G/=100;
+
   vector<int> nump (D), bonus (D);
+  int max_score=0;
   for (int i = 0; i < D; ++i) {
     cin >> nump.at (i) >> bonus.at (i);
+    bonus.at(i)/=100;
+    max_score+=(i+1)*nump.at(i)+bonus.at(i);
   }
-  int maxval = accumulate (nump.begin (), nump.end (), 0);
-  build_vcontainer (nump, bonus, D, G, 0, maxval, 0,0);
-  cout << maxval << endl;
+
+  vector<vector<int>> dp(D+1, vector<int>(max_score+1, INF));
+  dp.at(0).at(0)=0;
+
+  int nxt;
+  for (int i=0;i<D;++i){
+    for (int p=0;p<max_score+1;++p){
+      for (int j=0;j<nump.at(i);++j){
+        nxt=p+(i+1)*j;
+//        cout << nxt << ", " ;
+        if (nxt<=max_score){
+          dp.at(i+1).at(nxt)=min(dp.at(i+1).at(nxt), dp.at(i).at(p)+j);
+        }
+      }
+      nxt=p+(i+1)*nump.at(i)+bonus.at(i);
+//      cout << nxt << ", " << endl;
+      if (nxt<=max_score){
+        dp.at(i+1).at(nxt)=min(dp.at(i+1).at(nxt), dp.at(i).at(p)+nump.at(i));
+      }
+    }
+  }
+  int ans=INF;
+  for (int p=G;p<max_score+1;++p){
+    ans=min(ans,dp.at(D).at(p));
+  }
+  cout << ans << endl;
 }
+
