215abafab546aa50a0112ed7f0ada4db497fb6de
rooee <rujinami@gmail.com>
2023-08-18 16:36:17 +0900

    tessoku book A01

---
+++ Tessoku_book/A01/main.cpp
@@ -0,0 +1,13 @@
+// https://atcoder.jp/contests/tessoku-book/tasks/tessoku_book_a
+
+#include <bits/stdc++.h>
+using namespace std;
+
+int main () {
+
+  int N;
+  cin >> N;
+  cout << N*N << endl;
+  
+}
+
