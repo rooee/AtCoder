0ec391dfe41ece32e6893d825e315957ba3aad5e
rooee <rujinami@gmail.com>
2023-08-30 18:33:25 +0900

    tessoku book A09 (AC)

--- Tessoku_book/A09/main.cpp
+++ Tessoku_book/A09/main.cpp
@@ -8,21 +8,39 @@ int main () {
   int H, W,N;
   int A, B, C, D;
   cin >> H >> W >>N ;
-  vector <vector <int>> X(H, vector<int>(W,0));
+  vector <vector <int>> X(H+2, vector<int>(W+2,0));
+
   for (int i=0;i<N;++i){
     cin >> A >> B >> C >> D;
-    for (int h=A-1;h<C;++h){
-      for (int w=B-1;w<D;++w){
-        ++X.at(h).at(w);
-      }
+      X.at(A  ).at(B)  +=1;
+      X.at(C+1).at(D+1)+=1;
+      X.at(C+1).at(B  )-=1;
+      X.at(A  ).at(D+1)-=1;
+  }
+/*
+  for (int h=0;h<H+2;++h){
+    int w;
+    for (w=0;w<W+1;++w){
+      cout << X.at(h).at(w)<< " ";
     }
+    cout << X.at(h).at(w) << endl;
+  }
+*/
 
+  for (int w=1;w<W+2;++w){
+    for (int h=1;h<H+2;++h){
+      X.at(h).at(w)+=X.at(h-1).at(w);
+    }
+  }
+  for (int h=1;h<H+2;++h){
+    for (int w=1;w<W+2;++w){
+      X.at(h).at(w)+=X.at(h).at(w-1);
+    }
   }
 
-
-  for (int h=0;h<H;++h){
+  for (int h=1;h<H+1;++h){
     int w;
-    for (w=0;w<W-1;++w){
+    for (w=1;w<W;++w){
       cout << X.at(h).at(w)<< " ";
     }
     cout << X.at(h).at(w) << endl;
